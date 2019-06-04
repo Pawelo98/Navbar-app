2625121679f9e77991acb4dc29d3a9a10b0ace23
Paweł <pjanic98@gmail.com>
2019-06-05 00:26:41 +0200

    Add small fixes to riddleSolver and code refactoring

--- example.js
+++ example.js
@@ -3,7 +3,8 @@ const getRandomArbitrary = (min, max) => {
     return Math.random() * (max - min) + min;
 }
 
-// returns a board of sizes between 3 and 50 with values between 1 and 1000
+// returns a board of sizes between 3 and 50 with values between 1 and 10
+// changed values to section from 1 to 10 to make a riddle more reasonable
 const createBoard = () => {
     var matrix = [];
     var height = getRandomArbitrary(3, 50);
@@ -14,11 +15,11 @@ const createBoard = () => {
             matrix[i][j] = parseInt(getRandomArbitrary(1, 10));
         }
     }
-    console.dir(matrix);
     return matrix;
 }
 
 // takes a board and returns a solved board
+// both boards are being logged in the browser when launching the script
 const riddleSolver = (board) => { 
     iterate = true;
 
@@ -43,14 +44,11 @@ const riddleSolver = (board) => {
                 if(board[i][j]!=0) {
                     if (i+2 < board.length) {
                         if (board[i][j] == board[i+1][j] && board[i][j] == board[i+2][j]) {
+                            
                             // count items to drop
-                            var g = i + 3;
                             var licz = 3;
-                            if (i + 3 < board.length) {
-                                while(board[i][j]==board[g][j]) {
-                                    licz++;
-                                    g++;
-                                }
+                            for(var g=i+3; g<board.length; g++) {
+                                if(board[i][j]==board[g][j]) licz++;
                             }
 
                             // drop from the top
@@ -60,10 +58,13 @@ const riddleSolver = (board) => {
                                 } else 
                                 board[l][j] = 0;
                             }
+
                             // top are zeros
                             for (var m = 0; m < licz; m++) {
                                 board[0+m][j] = 0;
                             }
+
+                            // dont stop iterating from the top of the board
                             count=1;
                             i = board.length;
                             j = board[0].length;
@@ -73,13 +74,9 @@ const riddleSolver = (board) => {
                         if (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2]) {
 
                             // count items to drop
-                            var g = j + 3;
                             var licz = 3;
-                            if(j+3<board[0].length) {
-                                while (board[i][j] == board[i][g]) {
-                                    licz++;
-                                    g++;
-                                }
+                            for (var g=j+3; g<board[0].length; g++) {
+                                if (board[i][j] == board[i][g]) licz++;
                             }
 
                             // drop from the top
@@ -88,10 +85,13 @@ const riddleSolver = (board) => {
                                     board[l][j+m] = board[l-1][j+m];
                                 }
                             }
+
                             // top are zeros
                             for (var m = 0; m < licz; m++) {
                                 board[0][m+j] = 0;
                             }
+
+                            // dont stop iterating from the top of the board
                             count=1;
                             i = board.length;
                             j = board[0].length;
@@ -100,6 +100,8 @@ const riddleSolver = (board) => {
                 }
             }
         }
+        // dont stop iterating from the top of the board 
+        // if at least three items in a row with the same values were found
         if (count == 0) {
             iterate = false;
         }
@@ -108,8 +110,10 @@ const riddleSolver = (board) => {
     console.dir(board);
     return board;
 };
-/*
-const board = [
+
+// board from exercise example to check if it works well
+
+const boardExample = [
     [7, 7, 7, 5, 1, 7],
     [1, 8, 1, 1, 1, 4],
     [3, 2, 3, 9, 7, 6],
@@ -117,9 +121,14 @@ const board = [
     [1, 9, 3, 1, 8, 7],
     [5, 9, 2, 2, 4, 8]
 ];
-*/
 
+// randomly generate a board
 const boardCreated = createBoard();
 
-// usage of functions written above
+console.log("Board from the example: ");
+// riddleSolver for board from example given in the content of the task
+riddleSolver(boardExample);
+
+console.log("Randomly generated board: ");
+// riddleSolver for board created randomly by a function above
 riddleSolver(boardCreated);
